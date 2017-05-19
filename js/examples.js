16bc67d800f0b20e5e8241287c1a50adb64371b9
Hsin-Chieh Wu <dan40912@gmail.com>
2017-05-19 16:02:58 +0800

    add Facebook plugin
    fix anchor ,add Facebook in QA

--- js/examples.js
+++ js/examples.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 			$('#fullpage').fullpage({
-				anchors: ['section0', 'section1', 'section2', 'section3', 'section4'],
-				menu: '#menu',//equivalent to jQuery `easeOutBack` extracted from http://matthewlein.com/ceaser/
+				anchors: ['homepage', 'intro', 'compare', 'AC', 'DC','Q&A'],
+				menu: '#menu',
 				easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)','navigation': true,
 				'navigationPosition': 'right'
 			});
