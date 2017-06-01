a8e4150b8e839ce5731dcfb914c9a8023103e360
Hsin-Chieh Wu <dan40912@gmail.com>
2017-06-01 18:24:12 +0800

    add slide page for 14 DC
    add DC page and basic layout

--- js/examples.js
+++ js/examples.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 				anchors: ['homepage', 'intro', 'compare', 'AC', 'DC','Q&A'],
 				menu: '#menu',
 				easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)','navigation': true,
-				'navigationPosition': 'right'
+				'navigationPosition': 'right',controlArrows: true,scrollBar:true
 			});
 
 		});
