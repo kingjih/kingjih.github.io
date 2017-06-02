0dfec1fd3b15b51a3176989f081d28b8b7204ec0
Hsin-Chieh Wu <dan40912@gmail.com>
2017-06-02 10:59:38 +0800

    fix the anchor ,add bootstrap
    Fix anchor to 	anchors: ['homepage', 'intro', 'production', 'compare',
    'QA','contact’].Add bootstrap for layout

--- js/examples.js
+++ js/examples.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 			$('#fullpage').fullpage({
-				anchors: ['homepage', 'intro', 'compare', 'AC', 'DC','Q&A'],
+				anchors: ['homepage', 'intro', 'production', 'compare', 'QA','contact'],
 				menu: '#menu',
 				easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)','navigation': true,
 				'navigationPosition': 'right',controlArrows: true,scrollBar:true
